f88c1c3c17ae66b048bf40e641250f1bcb7a2f2b
FATEC <FATEC@DESKTOP-HR1F7SP>
2024-02-29 18:01:25 -0300

    porc

---
+++ controleversao/src/br/edu/fatecpg/controleversao/Porcentagem.java
@@ -0,0 +1,7 @@
+package br.edu.fatecpg.controleversao;
+
+public class Porcentagem {
+	public double regraDeTres(double N1,double N2,double N3) {
+		return (N2*N3)/N1;
+	}
+}
--- controleversao/src/br/edu/fatecpg/controleversao/Principal.java
+++ controleversao/src/br/edu/fatecpg/controleversao/Principal.java
@@ -8,11 +8,12 @@ public class Principal {
 		int operacao;
 		double N1,N2;
 		
+		Porcentagem porcento = new Porcentagem();
 		Calculadora calc = new Calculadora();
 		Scanner scan = new Scanner(System.in);
 		
 		while(true) {
-			System.out.println("Digite a operação desejada \n 1-Soma \n 2-Suntração \n 3-Multiplicação \n 4-Divisão");
+			System.out.println("Digite a operação desejada \n 1-Soma \n 2-Subtração \n 3-Multiplicação \n 4-Divisão \n 5-Sair");
 			operacao = scan.nextInt();
 			if(operacao == 5) {
 				break;
@@ -24,15 +25,20 @@ public class Principal {
 			switch(operacao){
 			case 1:
 				System.out.println("A soma dos valores é de: " + calc.soma(N1, N2));
+				break;
 			case 2:
-				System.out.println("A soma dos valores é de: " + calc.sub(N1, N2));
+				System.out.println("A subtração dos valores é de: " + calc.sub(N1, N2));
+				break;
 			case 3:
-				System.out.println("A soma dos valores é de: " + calc.mult(N1, N2));
+				System.out.println("A multiplicação dos valores é de: " + calc.mult(N1, N2));
+				break;
 			case 4: 
-				System.out.println("A soma dos valores é de: " + calc.div(N1, N2));
+				System.out.println("A divisão dos valores é de: " + calc.div(N1, N2));
+				break;
 			}
-			}
-	
+			
+		}
+		System.out.println(porcento.regraDeTres(100,100,20));
 		
 	}
 
